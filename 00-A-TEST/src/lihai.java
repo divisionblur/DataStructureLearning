490d6d0c85fb8649bce81463aed45a32f3a18cec
divisionblur <1842757284@qq.com>
2020-10-31 13:52:06 +0800

    2020.10.31

---
+++ 00-A-TEST/src/lihai.java
@@ -0,0 +1,9 @@
+/**
+ * @author joy division
+ * @date 2020/10/31 13:28
+ */
+public class lihai {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
